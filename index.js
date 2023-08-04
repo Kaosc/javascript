61ec01eafb9fc25698deb12360eb9dea2b880913
Kaosc <themelihbt@gmail.com>
2023-08-04 22:16:29 +0300

    update

---
+++ index.js
@@ -0,0 +1,26 @@
+const BASE_URL = "https://kaosc.github.io/javascript-projects/"
+
+const urls = [
+	"AirCnC",
+	"AirCnc Dashboard",
+	"Black Jack Game",
+	"Chrome Extention",
+	"GIF TV",
+	"Giphy Search Engine",
+	"Happy Hour",
+	"Sound Cloud Player",
+	"Passanger Count App",
+	"Sound Cloud Player",
+	"Tabletop Google Charts",
+]
+
+function listLinks() {
+	const section = document.getElementById("main")
+	urls.map((url) => {
+		const a = document.createElement("a")
+		a.innerText = url
+		a.href = `${BASE_URL}${url.split(" ").join("")}`
+		a.className = "button"
+		section.appendChild(a)
+	})
+}
